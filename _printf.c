d82a511f2db8a6a6f346581de0036058d68d00e9
Mngohol Etiennette Ashar <asharmngohol@gmail.com>
2023-12-05 06:20:19 -0800

    printf

--- _printf.c
+++ _printf.c
@@ -12,14 +12,14 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int number = 0;
+	int length = 0;
 
 	if (format == NULL)
 		return (-1);
 
 	va_start(args, format);
 
-	number = _print_format(format, args);
+	length = _print_format(format, args);
 	va_end(args);
-	return (number);
+	return (length);
 }
